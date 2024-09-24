6ea91bf52796f16a9f947f18dd68b0bda6112b05
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2024-09-24 13:29:14 +1000

    next update

--- components/MainContent.js
+++ components/MainContent.js
@@ -2,15 +2,11 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Card from "@/components/Card";
 import cardData from "@/data/data";
-import styles from "@/styles/MainContent.module.css"
-
+import styles from "@/styles/MainContent.module.css";
 
 export default function MainContent() {
   const displayInfo = cardData.map((info) => (
-    <Card
-      key={info.id}
-      info={info}
-    />
+    <Card key={info.id} info={info} />
   ));
 
   return (
